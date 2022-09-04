ab721e0f1d69f32c9d07308c49eecee13eb717c3
Winston Troughton <winston@troughton.net>
2022-09-03 23:04:03 -0400

    feat: Olivetti mode

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Winston Troughton"
+      user-mail-address "winston@troughton.net")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -21,9 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
+(setq doom-font (font-spec :family "Monaco" :size 13))
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
@@ -32,7 +30,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-gruvbox)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -42,10 +40,8 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Org/")
 
-;;
 (if (window-system)
-  (set-frame-size (selected-frame) 100 50))
-
+  (set-frame-size (selected-frame) 140 60))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -78,3 +74,71 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(use-package! olivetti
+  :ensure
+  :defer
+  :diminish
+  :config
+  (setq olivetti-body-width 0.65)
+  (setq olivetti-minimum-body-width 72)
+  (setq olivetti-recall-visual-line-mode-entry-state t)
+
+  (define-minor-mode wtroughton/olivetti-mode
+    "Toggle buffer-local `olivetti-mode' with additional parameters."
+    :init-value nil
+    :global nil
+    (if wtroughton/olivetti-mode
+        (progn
+          (display-line-numbers-mode -1)
+          (olivetti-mode 1)
+          (set-window-fringes (selected-window) 0 0)
+          (variable-pitch-mode 1)
+          (wtroughton/cursor-type-mode 1)
+          (wtroughton/hidden-mode-line-mode 1))
+      (display-line-numbers-mode 1)
+      (olivetti-mode -1)
+      (set-window-fringes (selected-window) nil) ; Use default width
+      (variable-pitch-mode -1)
+      (wtroughton/cursor-type-mode -1)
+      (wtroughton/hidden-mode-line-mode -1)))
+  :bind ("C-c o" . wtroughton/olivetti-mode))
+
+(use-package! frame
+  :commands wtroughton/cursor-type-mode
+  :config
+  (setq-default cursor-type 'box)
+  (setq-default cursor-in-non-selected-windows '(bar . 2))
+  (setq-default blink-cursor-blinks 50)
+  (setq-default blink-cursor-interval nil) ; 0.75 would be my choice
+  (setq-default blink-cursor-delay 0.2)
+
+  (blink-cursor-mode -1)
+
+  (define-minor-mode wtroughton/cursor-type-mode
+    "Toggle between static block and pulsing bar cursor."
+    :init-value nil
+    :global t
+    (if wtroughton/cursor-type-mode
+        (progn
+          (setq-local blink-cursor-interval 0.75
+                      cursor-type '(bar . 2)
+                      cursor-in-non-selected-windows 'hollow)
+          (blink-cursor-mode 1))
+      (dolist (local '(blink-cursor-interval
+                       cursor-type
+                       cursor-in-non-selected-windows))
+        (kill-local-variable `,local))
+      (blink-cursor-mode -1))))
+
+(use-package! emacs
+  :commands wtroughton/hidden-mode-line-mode
+  :config
+  (define-minor-mode wtroughton/hidden-mode-line-mode
+    "Toggle modeline visibility in the current buffer."
+    :init-value nil
+    :global nil
+    (if wtroughton/hidden-mode-line-mode
+        (setq-local mode-line-format nil)
+      (kill-local-variable 'mode-line-format)
+      (force-mode-line-update))))
